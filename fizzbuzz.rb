4b13ae5034c9975eab56495cafe3bbd48766575e
Kane Koo <kanekoo@gmail.com>
2019-02-20 20:23:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(var)
-  
+  if ((var % 3) == 0) 
+    return ("fizz")
+  else if 
